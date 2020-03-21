6bb405a268eb7e7fccb7d8a373c2dffe5ed4a6a6
Paul Nimusiima <xpaulnim@users.noreply.github.com>
2020-03-21 23:07:58 +0000

    Save bitmap with drawing to file

--- app/src/main/java/sample/activities/DrawingCanvasActivity.kt
+++ app/src/main/java/sample/activities/DrawingCanvasActivity.kt
@@ -1,8 +1,12 @@
 package sample.activities
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_drawing_canvas.*
 import sample.R
+import java.io.File
+import java.io.FileOutputStream
 
 class DrawingCanvasActivity : AppCompatActivity() {
 
@@ -10,6 +14,12 @@ class DrawingCanvasActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_drawing_canvas)
+
+        val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.png")
+
+        btnSaveBitmap.setOnClickListener {
+            customCanvasView.drawingBitmap!!.compress(Bitmap.CompressFormat.PNG,  100, FileOutputStream(file))
+        }
     }
 }
 
--- app/src/main/java/sample/views/CustomCanvasView.kt
+++ app/src/main/java/sample/views/CustomCanvasView.kt
@@ -1,25 +1,17 @@
 package sample.views;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
+import android.graphics.*
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintSet;
-
 import sample.R;
 
 class CustomCanvasView @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
+    attrs: AttributeSet? = null, // Required when view is inflated from XML
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
@@ -31,57 +23,93 @@ class CustomCanvasView @JvmOverloads constructor(
     val customMatrix = Matrix()
     val options = BitmapFactory.Options();
 
-    var bitmap: Bitmap?
+    private val imageBitmap: Bitmap?
+    private var myCanvas: Canvas? = null
+    var drawingBitmap: Bitmap? = null
 
     init {
         //        options.inJustDecodeBounds = true;
-        options.inSampleSize = 1;
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fifty_grand, options);
+        options.inSampleSize = 1
+        imageBitmap = BitmapFactory.decodeResource(resources, R.drawable.fifty_grand, options)
     }
+
+    var pointX = 300.0f
+    var pointY = 300.0f
+    val rect = RectF(10.0f, 10.0f, 50.0f, 20.0f)
+    var isHighlighting = false;
+
+    var initHighlightPosX: Float = 0.0f
+    var initHighlightPosY: Float = 0.0f
+
+    var moveHighlightPosX: Float = 0.0f
+    var moveHighlightPosY: Float = 0.0f
+
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas);
+        drawStuff(canvas)
+    }
+
+    private fun drawStuff(canvas: Canvas) {
+        paint.alpha = 255
+        paint.strokeWidth = 0f
+        paint.isAntiAlias = true
+        paint.color = Color.RED
+        paint.style = Paint.Style.FILL
 
 //        customMatrix.postRotate(20.0f);
-        canvas.drawBitmap(bitmap, customMatrix, paint);
+        canvas.drawBitmap(imageBitmap!!, customMatrix, paint)
 
-        paint.setAntiAlias(true);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(300.0f, 300.0f, 10.0f, paint);
+        if (isHighlighting) {
+            paint.alpha = 50
+            paint.strokeWidth = 30f
+            canvas.drawLine(
+                initHighlightPosX,
+                initHighlightPosY,
+                moveHighlightPosX,
+                moveHighlightPosY,
+                paint
+            )
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
-            printSamples(event);
-        } else {
-            Log.v(TAG, "actionEvent " + event.getActionMasked());
-        }
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                isHighlighting = true
+                initHighlightPosX = event.x
+                initHighlightPosY = event.y
 
-        return true
-    }
+                // start with all at inital click point
+                moveHighlightPosX = event.x
+                moveHighlightPosY = event.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                moveHighlightPosX = event.x
+                moveHighlightPosY = event.y
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                Log.v(TAG, "Drag finished. Anchoring highlight at last moveHighlightPosX")
 
-    fun printSamples(ev: MotionEvent) {
-        val historySize = ev.getHistorySize();
-        val pointerCount = ev.getPointerCount();
-
-        for (h in 0 until historySize) {
-            System.out.printf("At time %d:", ev.getHistoricalEventTime(h));
-
-            for (p in 0 until pointerCount) {
-                Log.v(
-                    TAG,
-                    "  pointer ${ev.getPointerId(p)}: (${ev.getHistoricalX(
-                        p,
-                        h
-                    )},${ev.getHistoricalY(p, h)})"
-                )
+                isHighlighting = false
             }
         }
 
-        Log.v(TAG, "At time ${ev.getEventTime()}:")
-        for (p in 0 until pointerCount) {
-            Log.v(TAG, "  pointer ${ev.getPointerId(p)}: (${ev.getX(p)},${ev.getY(p)})")
+        pointX = event.x
+        pointY = event.y
+
+        if (isHighlighting) {
+            invalidate()
+            Log.v(TAG, "This is ${this.width} by ${this.height}")
+
+            drawingBitmap = Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888)
+            myCanvas = Canvas(drawingBitmap!!)
+
+            drawStuff(myCanvas!!)
+
+            this.draw(myCanvas)
         }
+
+        return true
     }
 }
